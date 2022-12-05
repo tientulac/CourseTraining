8b998ba12af49f39cb94d5c3f61c7bce6a6b4d96
Nguyen Ngoc Tien <64886876+tientulac@users.noreply.github.com>
2022-12-05 08:04:14 +0700

    commit

--- CourseTraining/CourseTraining/Controllers/TraineePageController.cs
+++ CourseTraining/CourseTraining/Controllers/TraineePageController.cs
@@ -13,7 +13,32 @@ namespace CourseTraining.Controllers
         // GET: TraineePage
         private LinqDataContext db = new LinqDataContext();
 
-        public ActionResult Index(int id = 0)
+        public ActionResult Index()
+        {
+            //var _trainee = (from a in db.Trainees.Where(x => x.AccountId == id)
+            //                select new TraineeDTO
+            //                {
+            //                    Account = db.Accounts.Where(x => x.AccountId == a.AccountId).FirstOrDefault(),
+            //                    TraineeId = a.TraineeId,
+            //                    TraineeName = a.TraineeName,
+            //                    Dob = a.Dob,
+            //                    Image = a.Image,
+            //                    Gender = a.Gender,
+            //                    AccountId = a.AccountId,
+            //                    Education = a.Education,
+            //                    Phone = a.Phone,
+            //                    GenderName = a.Gender == true ? "Female" : "Male",
+            //                    UserName = db.Accounts.Where(x => x.AccountId == a.AccountId).FirstOrDefault().UserName ?? "",
+            //                    MainProgram = a.MainProgram,
+            //                    TOEICScore = a.TOEICScore,
+            //                    Detail = a.Detail,
+            //                    Address = a.Address
+            //                }).FirstOrDefault();
+            //ViewBag.Trainee = _trainee;
+            return View();
+        }
+
+        public ActionResult TraineeDetail(int id = 0)
         {
             var _trainee = (from a in db.Trainees.Where(x => x.AccountId == id)
                             select new TraineeDTO
@@ -38,14 +63,89 @@ namespace CourseTraining.Controllers
             return View();
         }
 
-        public ActionResult Course()
+        public ActionResult Course(int id = 0)
         {
+            var traineeId = db.Trainees.Where(x => x.AccountId == id).FirstOrDefault().TraineeId;
+            var listId = db.TraineeCourses.Where(x => x.TraineeId == traineeId).Select(c => c.CourseId);
+            var listCourse = (from a in db.Courses.Where(x => listId.Contains(x.CourseId))
+                              select new CourseDTO
+                              {
+                                  TraineeCourseId = db.TraineeCourses.Where(x => x.TraineeId == traineeId && x.CourseId == a.CourseId).FirstOrDefault().TraineeCourseId,
+                                  TraineeId = traineeId,
+                                  CourseId = a.CourseId,
+                                  CourseName = a.CourseName,
+                                  Image = a.Image,
+                                  Descrip = a.Descrip,
+                                  Status = a.Status,
+                                  CategoryId = a.CategoryId,
+                                  CategoryName = db.Categories.Where(x => x.CategoryId == a.CategoryId).FirstOrDefault().CategoryName ?? ""
+                              }).ToList();
+            var listTraineeCourse = (from a in db.Courses
+                              select new CourseDTO
+                              {
+                                  CourseId = a.CourseId,
+                                  CourseName = a.CourseName,
+                                  Image = a.Image,
+                                  Descrip = a.Descrip,
+                                  Status = a.Status,
+                                  CategoryId = a.CategoryId,
+                                  CategoryName = db.Categories.Where(x => x.CategoryId == a.CategoryId).FirstOrDefault().CategoryName ?? ""
+                              }).ToList();
+            ViewBag.ListCourse = listCourse;
+            ViewBag.TraineeId = traineeId;
+            ViewBag.ListTraineeCourse = listTraineeCourse;
             return View();
         }
 
-        public ActionResult Topic()
+        public ActionResult Topic(int id = 0)
         {
+            var traineeId = db.Trainees.Where(x => x.AccountId == id).FirstOrDefault().TraineeId;
+            var listId = db.TraineeTopics.Where(x => x.TraineeId == traineeId).Select(c => c.TopicId);
+            var listTopic = (from a in db.Topics.Where(x => listId.Contains(x.TopicId))
+                             select new TopicDTO
+                             {
+                                 TraineeTopicId = db.TraineeTopics.Where(x => x.TraineeId == traineeId && x.TopicId == a.TopicId).FirstOrDefault().TraineeTopicId,
+                                 TraineeId = traineeId,
+                                 TopicId = a.TopicId,
+                                 TopicName = a.TopicName,
+                                 Image = a.Image,
+                                 Descrip = a.Descrip,
+                                 Status = a.Status,
+                                 CategoryId = a.CategoryId,
+                                 CategoryName = db.Categories.Where(x => x.CategoryId == a.CategoryId).FirstOrDefault().CategoryName ?? ""
+                             }).ToList();
+            var listTraineeTopic = (from a in db.Topics
+                             select new TopicDTO
+                             {
+                                 TopicId = a.TopicId,
+                                 TopicName = a.TopicName,
+                                 Image = a.Image,
+                                 Descrip = a.Descrip,
+                                 Status = a.Status,
+                                 CategoryId = a.CategoryId,
+                                 CategoryName = db.Categories.Where(x => x.CategoryId == a.CategoryId).FirstOrDefault().CategoryName ?? "",
+                                 Author = a.Author
+                             }).ToList();
+            ViewBag.ListTopic = listTopic;
+            ViewBag.TraineeId = traineeId;
+            ViewBag.ListTraineeTopic = listTraineeTopic;
             return View();
+        }
+
+        public ActionResult DeleteCourse(int courseId)
+        {
+            var _course = db.TraineeCourses.Where(M => M.CourseId == courseId).FirstOrDefault();
+            db.TraineeCourses.DeleteOnSubmit(_course);
+            db.SubmitChanges();
+            return Json(new { success = true, data = _course }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult DeleteTopic(int topicId)
+        {
+            var _topic = db.TraineeTopics.Where(M => M.TopicId == topicId).FirstOrDefault();
+            db.TraineeTopics.DeleteOnSubmit(_topic);
+            db.SubmitChanges();
+            return Json(new { success = true, data = _topic }, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ CourseTraining/CourseTraining/Controllers/TrainerPageController.cs
@@ -0,0 +1,20 @@
+﻿using CourseTraining.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseTraining.Controllers
+{
+    public class TrainerPageController : Controller
+    {
+        // GET: TrainerPage
+        private LinqDataContext db = new LinqDataContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
